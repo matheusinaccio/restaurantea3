11d850e1b2f2e14208a9bd51156a5637814fe485
leonovo21 <leonardo94615@gmail.com>
2024-06-18 21:25:27 -0300

    Working I hope

--- src/a3_2024/Tela_inicio.java
+++ src/a3_2024/Tela_inicio.java
@@ -1,23 +1,18 @@
 package a3_2024;
 
-
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.Color;
-import javax.swing.JComboBox;
-import javax.swing.JScrollPane;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import java.awt.EventQueue;
 import java.awt.Font;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
-import javax.swing.border.CompoundBorder;
-import java.awt.SystemColor;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Tela_inicio extends JFrame {
 
@@ -26,6 +21,7 @@ public class Tela_inicio extends JFrame {
 	/**
 	 * Launch the application.
 	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -43,14 +39,16 @@ public class Tela_inicio extends JFrame {
 	 * Create the frame.
 	 */
 	public Tela_inicio() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\matheus\\Downloads\\file.png"));
 		
 		setAutoRequestFocus(false);
 		setBackground(Color.CYAN);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setFont(new Font("Stencil", Font.PLAIN, 11));
-		contentPane.setBackground(new Color(100, 149, 237));
+		contentPane.setBackground(new Color(79, 39, 0));
 		contentPane.setBorder(null);
 
 		setContentPane(contentPane);
@@ -58,21 +56,23 @@ public class Tela_inicio extends JFrame {
 		
 		//TEXTO RESTAURANTE
 		
-		JLabel lbl_restaurante = new JLabel("LES BLUE'S");
+		JLabel lbl_restaurante = new JLabel("~ LES BLUE'S ~");
+		lbl_restaurante.setBounds(10, 76, 764, 165);
 		lbl_restaurante.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_restaurante.setToolTipText("");
-		lbl_restaurante.setBounds(10, 132, 764, 65);
-		lbl_restaurante.setFont(new Font("Stencil", Font.BOLD, 39));
-		lbl_restaurante.setForeground(new Color(0, 0, 0));
+		lbl_restaurante.setFont(new Font("Agency FB", Font.BOLD | Font.ITALIC, 76));
+		lbl_restaurante.setForeground(new Color(255, 255, 255));
 		contentPane.add(lbl_restaurante);
+		
 		
 		//BOTAO INICIAR MENU
 		
 		JButton bntINICIAR = new JButton("INICIAR");
+		bntINICIAR.setBounds(319, 284, 156, 37);
 		bntINICIAR.setFont(new Font("Stencil", Font.PLAIN, 21));
 		bntINICIAR.setBorderPainted(false);
 		bntINICIAR.setForeground(Color.BLACK);
-		bntINICIAR.setBackground(new Color(100, 149, 237));
+		bntINICIAR.setBackground(new Color(255, 128, 0));
 		bntINICIAR.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Tela_Cardapio Tela_Cardapio = new Tela_Cardapio();
@@ -80,7 +80,11 @@ public class Tela_inicio extends JFrame {
 				dispose();
 			}
 		});
-		bntINICIAR.setBounds(319, 254, 156, 37);
 		contentPane.add(bntINICIAR);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Tela_inicio.class.getResource("/a3_2024/images/file.png")));
+		lblNewLabel.setBounds(405, 240, 514, 331);
+		contentPane.add(lblNewLabel);
 	}
 }
